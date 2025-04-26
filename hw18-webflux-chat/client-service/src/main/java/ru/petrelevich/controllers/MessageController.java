0c2d30877196d512a75faeb32a9142612481ce42
Вальтер Яков <y.valter@seuslab.ru>
2025-04-26 12:58:26 +0500

    hw18-webflux-chat fix

--- hw18-webflux-chat/client-service/src/main/java/ru/petrelevich/controllers/MessageController.java
+++ hw18-webflux-chat/client-service/src/main/java/ru/petrelevich/controllers/MessageController.java
@@ -22,6 +22,7 @@ public class MessageController {
     private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 
     private static final String TOPIC_TEMPLATE = "/topic/response.";
+    private static final String ROOM = "1408";
 
     private final WebClient datastoreClient;
     private final SimpMessagingTemplate template;
@@ -33,7 +34,7 @@ public class MessageController {
 
     @MessageMapping("/message.{roomId}")
     public void getMessage(@DestinationVariable("roomId") String roomId, Message message) {
-        if (!roomId.equals("1408")) {
+        if (!roomId.equals(ROOM)) {
             logger.info("get message:{}, roomId:{}", message, roomId);
             saveMessage(roomId, message).subscribe(msgId -> logger.info("message send id:{}", msgId));
 
@@ -86,7 +87,7 @@ public class MessageController {
     private Flux<Message> getMessagesByRoomId(long roomId) {
         return datastoreClient
                 .get()
-                .uri(String.format("/msg/%s", roomId))
+                .uri(Long.parseLong(ROOM) == roomId ? "/msg/" : String.format("/msg/%s", roomId))
                 .accept(MediaType.APPLICATION_NDJSON)
                 .exchangeToFlux(response -> {
                     if (response.statusCode().equals(HttpStatus.OK)) {
--- hw18-webflux-chat/datastore-service/src/main/java/ru/petrelevich/api/DataController.java
+++ hw18-webflux-chat/datastore-service/src/main/java/ru/petrelevich/api/DataController.java
@@ -21,8 +21,6 @@ public class DataController {
     private final DataStore dataStore;
     private final Scheduler workerPool;
 
-    private final String magicRoom = "1408";
-
     public DataController(DataStore dataStore, Scheduler workerPool) {
         this.dataStore = dataStore;
         this.workerPool = workerPool;
@@ -30,9 +28,6 @@ public class DataController {
 
     @PostMapping(value = "/msg/{roomId}")
     public Mono<Long> messageFromChat(@PathVariable("roomId") String roomId, @RequestBody MessageDto messageDto) {
-        if (magicRoom.equals(roomId)) {
-            return Mono.just(0L);
-        }
         var messageStr = messageDto.messageStr();
 
         var msgId = Mono.just(new Message(null, roomId, messageStr))
@@ -51,8 +46,17 @@ public class DataController {
     public Flux<MessageDto> getMessagesByRoomId(@PathVariable("roomId") String roomId) {
         return Mono.just(roomId)
                 .doOnNext(room -> log.info("getMessagesByRoomId, room:{}", room))
-                .flatMapMany(s ->
-                        magicRoom.equals(roomId) ? dataStore.loadMessagesFromAllRooms() : dataStore.loadMessages(s))
+                .flatMapMany(s -> dataStore.loadMessages(s))
+                .map(message -> new MessageDto(message.msgText()))
+                .doOnNext(msgDto -> log.info("msgDto:{}", msgDto))
+                .subscribeOn(workerPool);
+    }
+
+    @GetMapping(value = "/msg/", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<MessageDto> getMessagesByRoomId() {
+        return Mono.just("all")
+                .doOnNext(room -> log.info("getMessagesFromAllRooms"))
+                .flatMapMany(s -> dataStore.loadMessagesFromAllRooms())
                 .map(message -> new MessageDto(message.msgText()))
                 .doOnNext(msgDto -> log.info("msgDto:{}", msgDto))
                 .subscribeOn(workerPool);
